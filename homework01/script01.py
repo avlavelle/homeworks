e7abba49569395fb4ef392d080a8344c34aa3cc6
Anna Victoria Lavelle <annavictoria.lavelle@austin.utexas.edu>
2023-01-20 14:59:48 -0600

    prints five longest words from words

---
+++ homework01/script01.py
@@ -0,0 +1,13 @@
+#Anna Victoria Lavelle
+#AVL578
+#1/20/2023
+#script01.py
+
+words = []
+
+with open('/home/avlav/words', 'r') as f:
+    words = f.read().splitlines()
+
+words.sort(key = len, reverse = True)
+
+print(words[:5])
